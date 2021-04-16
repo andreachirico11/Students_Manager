16dfde3c0a7967f2db3fe21c4b58627177068a47
Andrea Chirico <andreachirico11@gmail.com>
2021-04-16 12:41:36 +0200

    update sidebar routing

--- frontend/src/app/main-page/sidebar/sidebar.component.spec.ts
+++ frontend/src/app/main-page/sidebar/sidebar.component.spec.ts
@@ -1,32 +1,31 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { Directive, Input } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { of } from 'rxjs';
 import { DataService } from 'src/app/main-page/data-service/data.service';
 import { Student } from 'src/app/shared/models/Student';
 import { MaterialModule } from '../../material.module';
 import { SidebarComponent } from './sidebar.component';
 
-@Directive({
-  selector: '[routerLink]',
-})
-export class FakeRouterLink {
-  @Input()
-  routerLink;
-}
-
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
   let dbService: DataService;
+  let router: Router;
 
   const fakeStudentsDb = [new Student('gianni', 'gianno', '', new Date(), '', '', [], [], '', '1')],
     getListOptions = () => fixture.debugElement.queryAllNodes(By.css('mat-list-option'));
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SidebarComponent, FakeRouterLink],
-      imports: [HttpClientTestingModule, MaterialModule],
+      declarations: [SidebarComponent],
+      imports: [HttpClientTestingModule, MaterialModule, RouterModule.forRoot([])],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
   });
 
@@ -35,6 +34,7 @@ describe('SidebarComponent', () => {
       fixture = TestBed.createComponent(SidebarComponent);
       component = fixture.componentInstance;
       dbService = TestBed.inject(DataService);
+      router = TestBed.inject(Router);
       fixture.detectChanges();
     })
   );
@@ -48,5 +48,24 @@ describe('SidebarComponent', () => {
     component.ngOnInit();
     fixture.detectChanges();
     expect(getListOptions().length).toBe(1);
+  });
+
+  it('should navigate to the right url', () => {
+    spyOn(dbService, 'getStudents').and.returnValue(of(fakeStudentsDb));
+    component.ngOnInit();
+    fixture.detectChanges();
+    const spy = spyOn(router, 'navigate');
+    getListOptions()[0].nativeNode.click();
+    expect(spy).toHaveBeenCalledOnceWith(['1']);
+  });
+
+  it('should navigate to the home url if the previous link is pressed', () => {
+    spyOn(dbService, 'getStudents').and.returnValue(of(fakeStudentsDb));
+    component.ngOnInit();
+    fixture.detectChanges();
+    const spy = spyOn(router, 'navigate');
+    getListOptions()[0].nativeNode.click();
+    getListOptions()[0].nativeNode.click();
+    expect(spy).toHaveBeenCalledWith(['1']);
   });
 });
--- frontend/src/app/main-page/sidebar/sidebar.component.ts
+++ frontend/src/app/main-page/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/main-page/data-service/data.service';
 import { Student } from 'src/app/shared/models/Student';
@@ -10,10 +11,26 @@ import { Student } from 'src/app/shared/models/Student';
 })
 export class SidebarComponent implements OnInit {
   public students: Observable<Student[]>;
+  private actualStudentIdLoaded: string = '';
 
-  constructor(private dbService: DataService) {}
+  constructor(
+    private dbService: DataService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.students = this.dbService.getStudents();
+  }
+
+  navigateToStudent(studentId: string): void {
+    if (this.actualStudentIdLoaded === '') {
+      this.actualStudentIdLoaded = studentId;
+    } else if (this.actualStudentIdLoaded === studentId) {
+      this.actualStudentIdLoaded = '';
+    } else {
+      this.actualStudentIdLoaded = studentId;
+    }
+    this.router.navigate([this.actualStudentIdLoaded]);
   }
 }
