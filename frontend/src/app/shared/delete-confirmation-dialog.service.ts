e8fef6c5ddbf1157f402a8f2296b9bf4bdc8f8c7
andreachirico11 <andreachirico11@gmail.com>
2021-08-25 07:27:16 +0200

    bug 3 delete message

--- frontend/src/app/shared/delete-confirmation-dialog.service.ts
+++ frontend/src/app/shared/delete-confirmation-dialog.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
-import { first } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 @Injectable({
@@ -11,24 +11,21 @@ import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-
 export class DeleteConfirmationDialogService {
   private translations: { [key: string]: string };
 
-  constructor(private dialog: MatDialog, private translator: TranslateService) {
-    this.translator
-      .get('DIALOG')
-      .pipe(first())
-      .subscribe((translations) => {
-        this.translations = translations;
-      });
-  }
+  constructor(private dialog: MatDialog, private translator: TranslateService) {}
 
-  open(dialogId?: string): Observable<boolean> {
-    const dialogRef = dialogId
-      ? this.dialog.open(ConfirmationDialogComponent, {
-          id: 'STUDENT_DELETE_DIALOG',
-        })
-      : this.dialog.open(ConfirmationDialogComponent);
-    dialogRef.componentInstance.dialogTitle = this.translations['DELETE_QUEST'];
-    dialogRef.componentInstance.successBtnLabel = this.translations['YES'];
-    dialogRef.componentInstance.unsuccessBtnLabel = this.translations['NO'];
-    return dialogRef.afterClosed();
+  open(dialogId?: string): Observable<any> {
+    return this.translator.get('DIALOG').pipe(
+      switchMap((translations: { [key: string]: string }) => {
+        const dialogRef = dialogId
+          ? this.dialog.open(ConfirmationDialogComponent, {
+              id: 'STUDENT_DELETE_DIALOG',
+            })
+          : this.dialog.open(ConfirmationDialogComponent);
+        dialogRef.componentInstance.dialogTitle = translations['DELETE_QUEST'];
+        dialogRef.componentInstance.successBtnLabel = translations['YES'];
+        dialogRef.componentInstance.unsuccessBtnLabel = translations['NO'];
+        return dialogRef.afterClosed();
+      })
+    );
   }
 }
