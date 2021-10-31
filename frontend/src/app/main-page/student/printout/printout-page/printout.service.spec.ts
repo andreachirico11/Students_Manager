1a4187d07b3c9de5d2653077e7aefce1443e4b18
andreachirico11 <andreachirico11@gmail.com>
2021-10-31 18:28:44 +0100

    fix testing bug

--- frontend/src/app/main-page/student/printout/printout-page/printout.service.spec.ts
+++ frontend/src/app/main-page/student/printout/printout-page/printout.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { PrintoutService } from './printout.service';
@@ -6,7 +7,9 @@ describe('PrintoutService', () => {
   let service: PrintoutService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(PrintoutService);
   });
 
