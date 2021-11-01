9fbcd1d0021c8254586a1d659fd125e4ed7b8f3a
andreachirico11 <andreachirico11@gmail.com>
2021-11-01 18:05:12 +0100

    compiled folder removed

--- backend/compiled/models/interfaces/TypeOfPayment.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PaymentTypeValues = void 0;
-var money = 'money', transfer = 'transfer', atm = 'atm';
-exports.PaymentTypeValues = [money, transfer, atm];
-//# sourceMappingURL=TypeOfPayment.js.map
--- backend/compiled/utils/httpRespGenerator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateHttpRes = void 0;
-var httpResponse_1 = require("../models/httpResponse");
-var generateHttpRes = function (res, code, title, payload) {
-    res.status(code).json(new httpResponse_1.HttpResponse(title, payload));
-};
-exports.generateHttpRes = generateHttpRes;
-//# sourceMappingURL=httpRespGenerator.js.map
