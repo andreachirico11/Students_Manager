b6c5c5d0d6c7ef685b6270f8262eaf125fcbdc92
andreachirico11 <andreachirico11@gmail.com>
2021-11-01 10:18:59 +0100

    pdf serving doesn't works

--- backend/models/interfaces/IRequests.ts
+++ backend/models/interfaces/IRequests.ts
@@ -5,10 +5,18 @@ export interface IBackendRequest<T> extends IRequest {
   body: T;
 }
 
+export interface IPdfRequest extends IRequest {
+  headers: pdfHeaders;
+}
+
 export interface IRequest extends Request {
   headers: headersWithAuth;
 }
 
 interface headersWithAuth extends IncomingHttpHeaders {
   'auth-token': string;
+}
+
+interface pdfHeaders extends headersWithAuth {
+  // TODO
 }
