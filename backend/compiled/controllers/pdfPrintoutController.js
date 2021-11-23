c6d34973ef7e6674fabac4ee4cfea12d97a61550
andreachirico11 <andreachirico11@gmail.com>
2021-11-23 07:51:35 +0100

    fix bug after merge

--- backend/compiled/controllers/pdfPrintoutController.js
+++ backend/compiled/controllers/pdfPrintoutController.js
@@ -1,27 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPdf = void 0;
-// export function postReceipt(req: IBackendRequest<IReceipt>, res: Response) {
-//   let receiptToSend: IReceipt;
-//   ReceiptModelBuilder(req.body, req.params.studentId)
-//     .then((receipt) => {
-//       if (receipt) {
-//         receiptToSend = receipt;
-//         return StudentModel.updateOne(
-//           { _id: req.params.studentId },
-//           { $push: { receiptIds: receipt._id } }
-//         );
-//       }
-//       throw new Error();
-//     })
-//     .then(() => generateHttpRes(res, 200, ReceiptMessages.receipt_created, receiptToSend))
-//     .catch((err) => generateHttpRes(res, 500, ServerMessages.creation_error));
-// }
+var path_1 = require("path");
 function getPdf(eq, res) {
-    console.log('getting pdf');
-    // modify using path package
     res.setHeader('Content-Type', 'application/pdf');
-    res.sendFile(__dirname + '/pdf-views/pdf-sample.pdf', function (err) {
+    // the path is calculated from inside the compiled folder
+    res.sendFile((0, path_1.join)(__dirname, '..', '..', 'pdf-views', 'pdf-sample.pdf'), function (err) {
         if (err) {
             console.log('err: ', err);
         }
