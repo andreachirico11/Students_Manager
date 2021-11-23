d448d9a5981d305bb83cdca24644c7ed2098ef9c
andreachirico11 <andreachirico11@gmail.com>
2021-11-23 08:30:07 +0100

    #33 installed packages and fetched html

--- backend/compiled/controllers/pdfPrintoutController.js
+++ backend/compiled/controllers/pdfPrintoutController.js
@@ -2,9 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPdf = void 0;
 var path_1 = require("path");
+var fs_1 = require("fs");
 function getPdf(eq, res) {
-    res.setHeader('Content-Type', 'application/pdf');
     // the path is calculated from inside the compiled folder
+    res.setHeader('Content-Type', 'application/pdf');
+    (0, fs_1.readFile)((0, path_1.join)(__dirname, '..', '..', 'pdf-views', 'html-sample.html'), function (err, file) {
+        if (err) {
+            console.log('E R R O R:', err);
+        }
+        console.log(file);
+    });
+    return;
     res.sendFile((0, path_1.join)(__dirname, '..', '..', 'pdf-views', 'pdf-sample.pdf'), function (err) {
         if (err) {
             console.log('err: ', err);
--- backend/controllers/pdfPrintoutController.ts
+++ backend/controllers/pdfPrintoutController.ts
@@ -1,10 +1,22 @@
 import { Response } from 'express';
 import { join } from 'path';
 import { IBackendRequest, IPdfRequest } from '../models/interfaces/IRequests';
+import { create } from 'html-pdf';
+import { readFile, readFileSync } from 'fs';
 
 export function getPdf(eq: IBackendRequest<IPdfRequest>, res: Response) {
-  res.setHeader('Content-Type', 'application/pdf');
   // the path is calculated from inside the compiled folder
+  res.setHeader('Content-Type', 'application/pdf');
+
+  readFile(join(__dirname, '..', '..', 'pdf-views', 'html-sample.html'), function (err, file) {
+    if (err) {
+      console.log('E R R O R:', err);
+    }
+    console.log(file);
+  });
+
+  return;
+
   res.sendFile(join(__dirname, '..', '..', 'pdf-views', 'pdf-sample.pdf'), function (err) {
     if (err) {
       console.log('err: ', err);
