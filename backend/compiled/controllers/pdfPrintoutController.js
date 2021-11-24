c6b089be95b1e807916d48bad9fce252b1f0620a
andreachirico11 <andreachirico11@gmail.com>
2021-11-24 21:04:59 +0100

    closes #34

--- backend/compiled/controllers/pdfPrintoutController.js
+++ backend/compiled/controllers/pdfPrintoutController.js
@@ -5,21 +5,36 @@ var fs_1 = require("fs");
 var html_pdf_1 = require("html-pdf");
 var path_1 = require("path");
 var ejs_1 = require("ejs");
+var receiptModel_1 = require("../models/receiptModel");
+var httpRespGenerator_1 = require("../utils/httpRespGenerator");
 function getPdf(eq, res) {
     // the path is calculated from inside the compiled folder
     res.setHeader('Content-Type', 'application/pdf');
     var fileName = 'pdf-wella.pdf';
-    (0, ejs_1.renderFile)((0, path_1.join)(__dirname, '..', '..', 'pdf-views', 'ejs-sample.ejs'), { title: 'bella li' }, function (err, file) {
-        handleError(err, 'ejs');
-        (0, html_pdf_1.create)(file).toFile(fileName, function (err, file) {
-            handleError(err, 'creating');
-            res.setHeader('file-name', 'mega-printout');
-            res.sendFile(file.filename, function (err) {
-                handleError(err, 'sending');
-                (0, fs_1.unlinkSync)(fileName);
+    receiptModel_1.ReceiptModel.find()
+        .then(function (receipts) {
+        console.log(receipts);
+        // dates have to be parsed
+        (0, ejs_1.renderFile)((0, path_1.join)(__dirname, '..', '..', 'pdf-views', 'full-table.ejs'), { receipts: receipts }, function (err, file) {
+            handleError(err, 'ejs');
+            (0, html_pdf_1.create)(file, {
+                format: 'A4',
+                orientation: 'portrait',
+                border: {
+                    top: '50px',
+                    bottom: '50px',
+                },
+            }).toFile(fileName, function (err, file) {
+                handleError(err, 'creating');
+                res.setHeader('file-name', 'mega-printout');
+                res.sendFile(file.filename, function (err) {
+                    handleError(err, 'sending');
+                    (0, fs_1.unlinkSync)(fileName);
+                });
             });
         });
-    });
+    })
+        .catch(function () { return (0, httpRespGenerator_1.generateHttpRes)(res, 500, 'cannot create pdf'); });
 }
 exports.getPdf = getPdf;
 function handleError(err, msg) {
--- backend/controllers/pdfPrintoutController.ts
+++ backend/controllers/pdfPrintoutController.ts
@@ -4,27 +4,43 @@ import { create } from 'html-pdf';
 import { join } from 'path';
 import { IBackendRequest, IPdfRequest } from '../models/interfaces/IRequests';
 import { renderFile } from 'ejs';
+import { ReceiptModel } from '../models/receiptModel';
+import { IReceipt } from '../models/interfaces/Receipt';
+import { generateHttpRes } from '../utils/httpRespGenerator';
 
 export function getPdf(eq: IBackendRequest<IPdfRequest>, res: Response) {
   // the path is calculated from inside the compiled folder
   res.setHeader('Content-Type', 'application/pdf');
   const fileName = 'pdf-wella.pdf';
 
-  renderFile(
-    join(__dirname, '..', '..', 'pdf-views', 'ejs-sample.ejs'),
-    { title: 'bella li' },
-    function (err, file) {
-      handleError(err, 'ejs');
-      create(file).toFile(fileName, function (err, file) {
-        handleError(err, 'creating');
-        res.setHeader('file-name', 'mega-printout');
-        res.sendFile(file.filename, function (err) {
-          handleError(err, 'sending');
-          unlinkSync(fileName);
-        });
-      });
-    }
-  );
+  ReceiptModel.find()
+    .then((receipts: IReceipt[]) => {
+      console.log(receipts);
+      // dates have to be parsed
+      renderFile(
+        join(__dirname, '..', '..', 'pdf-views', 'full-table.ejs'),
+        { receipts },
+        function (err, file) {
+          handleError(err, 'ejs');
+          create(file, {
+            format: 'A4',
+            orientation: 'portrait',
+            border: {
+              top: '50px',
+              bottom: '50px',
+            },
+          }).toFile(fileName, function (err, file) {
+            handleError(err, 'creating');
+            res.setHeader('file-name', 'mega-printout');
+            res.sendFile(file.filename, function (err) {
+              handleError(err, 'sending');
+              unlinkSync(fileName);
+            });
+          });
+        }
+      );
+    })
+    .catch(() => generateHttpRes(res, 500, 'cannot create pdf'));
 }
 
 function handleError(err, msg) {
