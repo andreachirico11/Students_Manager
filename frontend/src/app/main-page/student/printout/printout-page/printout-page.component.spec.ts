daeed800306ebe1014f59803e5ddaefa079e73f2
andreachirico11 <andreachirico11@mgmail.com>
2022-02-10 12:25:26 +0100

    \#47 fixed test bug

--- frontend/src/app/main-page/student/printout/printout-page/printout-page.component.spec.ts
+++ frontend/src/app/main-page/student/printout/printout-page/printout-page.component.spec.ts
@@ -7,6 +7,7 @@ import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
 import { MaterialModule } from 'src/app/material.module';
 import { ReceiptsFilters } from 'src/app/shared/models/receiptsFilters';
 import { IStudentPdfReqBody } from '../IStudentPdfReqBody';
@@ -57,55 +58,43 @@ describe('PrintoutPageComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it(
-    'should show dateFields when daterange is selected',
-    waitForAsync(() => {
-      expect(areDateRangesVisibles()).toBeFalse();
-      component.form.get('filters').setValue([ReceiptsFilters.dateRange]);
-      component.form.updateValueAndValidity();
-      fixture.detectChanges();
-      expect(areDateRangesVisibles()).toBeTrue();
-    })
-  );
+  it('should show dateFields when daterange is selected', waitForAsync(() => {
+    expect(areDateRangesVisibles()).toBeFalse();
+    component.form.get('filters').setValue([ReceiptsFilters.dateRange]);
+    component.form.updateValueAndValidity();
+    fixture.detectChanges();
+    expect(areDateRangesVisibles()).toBeTrue();
+  }));
 
-  it(
-    'should hide dateFields when daterange is deselected',
-    waitForAsync(() => {
-      component.form.get('filters').setValue([ReceiptsFilters.dateRange]);
-      component.form.updateValueAndValidity();
-      fixture.detectChanges();
-      expect(areDateRangesVisibles()).toBeTrue();
-      component.form.get('filters').setValue([]);
-      component.form.updateValueAndValidity();
-      fixture.detectChanges();
-      expect(areDateRangesVisibles()).toBeFalse();
-    })
-  );
+  it('should hide dateFields when daterange is deselected', waitForAsync(() => {
+    component.form.get('filters').setValue([ReceiptsFilters.dateRange]);
+    component.form.updateValueAndValidity();
+    fixture.detectChanges();
+    expect(areDateRangesVisibles()).toBeTrue();
+    component.form.get('filters').setValue([]);
+    component.form.updateValueAndValidity();
+    fixture.detectChanges();
+    expect(areDateRangesVisibles()).toBeFalse();
+  }));
 
-  it(
-    'should hide dateFields when thisYear is selected',
-    waitForAsync(() => {
-      component.form.get('filters').setValue([ReceiptsFilters.dateRange]);
-      component.form.updateValueAndValidity();
-      fixture.detectChanges();
-      expect(areDateRangesVisibles()).toBeTrue();
-      component.form.get('filters').setValue([ReceiptsFilters.thisYear]);
-      component.form.updateValueAndValidity();
-      fixture.detectChanges();
-      expect(areDateRangesVisibles()).toBeFalse();
-    })
-  );
+  it('should hide dateFields when thisYear is selected', waitForAsync(() => {
+    component.form.get('filters').setValue([ReceiptsFilters.dateRange]);
+    component.form.updateValueAndValidity();
+    fixture.detectChanges();
+    expect(areDateRangesVisibles()).toBeTrue();
+    component.form.get('filters').setValue([ReceiptsFilters.thisYear]);
+    component.form.updateValueAndValidity();
+    fixture.detectChanges();
+    expect(areDateRangesVisibles()).toBeFalse();
+  }));
 
-  it(
-    'should remove thisYear when thisMonth is selected',
-    waitForAsync(() => {
-      component.form.get('filters').setValue([ReceiptsFilters.thisYear]);
-      component.form.updateValueAndValidity();
-      component.form.get('filters').setValue([ReceiptsFilters.thisMonth]);
-      component.form.updateValueAndValidity();
-      expect(component.form.get('filters').value).toEqual([ReceiptsFilters.thisMonth]);
-    })
-  );
+  it('should remove thisYear when thisMonth is selected', waitForAsync(() => {
+    component.form.get('filters').setValue([ReceiptsFilters.thisYear]);
+    component.form.updateValueAndValidity();
+    component.form.get('filters').setValue([ReceiptsFilters.thisMonth]);
+    component.form.updateValueAndValidity();
+    expect(component.form.get('filters').value).toEqual([ReceiptsFilters.thisMonth]);
+  }));
 
   it('should send the correct data', () => {
     const data: IStudentPdfReqBody = {
@@ -172,7 +161,7 @@ describe('PrintoutPageComponent', () => {
   });
 
   it('should send a request to service with student id param', () => {
-    const spy = spyOn(printoutService, 'getBlankReceipt').and.callThrough();
+    const spy = spyOn(printoutService, 'getBlankReceipt').and.returnValue(of(null));
     fixture.debugElement.query(By.css('.blank-page-btn')).nativeElement.click();
     expect(spy).toHaveBeenCalledWith('abc');
   });
