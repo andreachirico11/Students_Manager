4a86fc7a711825856c7e3b6d96a5aff73aac3e99
andreachirico11 <57347299+andreachirico11@users.noreply.github.com>
2022-11-27 13:13:08 +0100

    chore: updated api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  dbUrl: 'https://student-manager-1.herokuapp.com/api/',
+  dbUrl: 'https://student-manager-2.onrender.com',
   idleTimeout: 86400000,
 };
